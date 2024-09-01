3737685349713975a14cb600d3cfed2f0fcd751d
Kionish Binns <kionishbinns@Kionishs-Laptop.lan>
2024-09-01 19:38:27 -0400

    updates to week6 tasks.js

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,31 +1,50 @@
+const Task = require(`../models/task`);
+const asyncWrapper = require(`../middleware/async`);
+const { createCustomError } = require(`../errors/custom-error`);
 
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+  
+});
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-// const createTask = (req, res) => {
-//     res.send('create task')
-// } 
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`No task with ID: ${taskID}`, 404));
+  } else {
+    res.status(200).json({ task });
+  }
+});
 
-const createTask = (req, res) => {
-    res.json(req.body)
-}
-// const getTask = (req, res) => {
-//     res.send('get single task')
-// }
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
-}
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`No task with ID: ${taskID}`, 404));
+  } else {
+    res.status(200).json({ msg: `Success. Task ${taskID} Deleted.` });
+  }
+});
 
-const updateTask = (req, res) => {
-    res.send('update task')
-}
-
-const deleteTask = (req, res) => {
-    res.send('delete task')
-}
-
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return next(createCustomError(`No task with ID: ${taskID}`, 404));
+  } else {
+    res.status(200).json({ task });
+  }
+});
 
 module.exports = {
     getAllTasks,
